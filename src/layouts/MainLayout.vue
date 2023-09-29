ce592c31e3765c5c03f079c761278ead839930f4
emmanueltlom <nwabuodafiemmanuel@gmail.com>
2023-09-29 17:12:34 +0100

    initial commits

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,43 +1,67 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+    <header class="header row justify-between items-center">
+      <div class="container">
+        <div class="navbar">
+          <div class="">
+            <router-link to="/">
+              <img src="../assets/logo.svg" alt="" />
+            </router-link>
+          </div>
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+          <nav>
+            <ul>
+              <li>
+                <q-btn @click="goto('timeline')" class="nav_item" flat no-caps>
+                  Home
+                </q-btn>
+              </li>
+              <li>
+                <q-btn @click="goto('timeline')" class="nav_item" flat no-caps>
+                  Introduction
+                </q-btn>
+              </li>
+              <li>
+                <q-btn @click="goto('overview')" class="nav_item" flat no-caps>
+                  The winners
+                </q-btn>
+              </li>
+              <li>
+                <q-btn @click="goto('faq')" class="nav_item" flat no-caps>
+                  Event
+                </q-btn>
+              </li>
+              <li>
+                <q-btn @click="goto('faq')" class="nav_item" flat no-caps>
+                  Gallery
+                </q-btn>
+              </li>
+              <li>
+                <q-btn @click="goto('faq')" class="nav_item" flat no-caps>
+                  Meet the Judges
+                </q-btn>
+              </li>
+            </ul>
+          </nav>
 
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
+          <q-btn
+            :to="{ name: 'contact' }"
+            class="nav_item contact"
+            flat
+            no-caps
+          >
+            Partner with us
+          </q-btn>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
+          <q-btn @click="toggleNav" flat no-caps class="menu">
+            <img src="../assets/menu.svg" alt="" />
+          </q-btn>
+          <q-btn @click="toggleNav" flat no-caps class="close">
+            <img src="../assets/cancel.svg" alt="" />
+          </q-btn>
+        </div>
+      </div>
+    </header>
 
     <q-page-container>
       <router-view />
@@ -46,71 +70,197 @@
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
-  name: 'MainLayout',
+  name: "MainLayout",
 
-  components: {
-    EssentialLink
-  },
-
-  setup () {
-    const leftDrawerOpen = ref(false)
+  setup() {
+    const leftDrawerOpen = ref(false);
 
     return {
-      essentialLinks: linksList,
       leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+
+  // mounted() {
+  //   console.log(this.$router.currentRoute.value.name);
+  // },
+
+  watch: {
+    $route(to, from) {
+      let nav = document.querySelector(".header");
+      let body = document.querySelector("body");
+      console.log(to, from);
+      if (to.name !== from.name) {
+        body.classList.remove("no_scroll");
+        nav.classList.remove("active");
       }
-    }
-  }
-})
+      // console.log("first");
+    },
+  },
+  methods: {
+    toggleNav() {
+      let nav = document.querySelector(".header");
+      let body = document.querySelector("body");
+
+      if (nav.classList.contains("active")) {
+        nav.classList.remove("active");
+        nav.classList.add("remove");
+        body.classList.remove("no_scroll");
+      } else {
+        nav.classList.add("active");
+        body.classList.add("no_scroll");
+      }
+    },
+
+    goto(arg) {
+      let body = document.querySelector("body");
+      let nav = document.querySelector(".header");
+      body.classList.remove("no_scroll");
+      nav.classList.remove("active");
+      document.getElementById(arg).scrollIntoView({ behavior: "smooth" });
+    },
+  },
+});
 </script>
+
+<style scoped>
+.close {
+  display: none;
+  position: relative;
+}
+.close img:first-of-type {
+  width: 45px;
+  height: 45px;
+}
+.close img:last-of-type {
+  width: 22px;
+  height: 22px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+header.remove {
+  animation: slideOut 1000ms ease-out forwards;
+}
+header.active {
+  animation: slideIn 500ms ease-out forwards;
+}
+
+@keyframes slideIn {
+  0% {
+    opacity: 0;
+    transform: translateX(-250%);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+@keyframes slideOut {
+  0% {
+    opacity: 0;
+    transform: translateX(-250);
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+@media (min-width: 850px) {
+  .menu {
+    display: none;
+  }
+}
+@media (max-width: 1000px) {
+  .navbar nav ul {
+    gap: 1rem;
+  }
+}
+@media (max-width: 850px) {
+  nav ul {
+    display: none;
+  }
+
+  header.active {
+    border-radius: 8px;
+    border: 0.5px solid rgba(255, 255, 255, 0.04);
+    background: #150e28;
+    backdrop-filter: blur(30px);
+    height: 100vh;
+    overflow: hidden;
+    position: relative;
+    animation: slideIn 500ms ease-out forwards;
+  }
+
+  header.active nav ul {
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+  header.active .logo {
+    display: none;
+  }
+  header.active .navbar nav ul {
+    gap: 2rem;
+  }
+  header.active .nav_item {
+    color: #fff;
+    font-family: "Inter", sans-serif;
+    font-size: 22px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    letter-spacing: -1px;
+  }
+  header.active .close {
+    display: block;
+  }
+  header.active .menu {
+    display: none;
+  }
+}
+@media (max-width: 500px) {
+  header.active .nav_item {
+    color: #fff;
+    font-family: "Inter", sans-serif;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    letter-spacing: -1px;
+  }
+
+  header.active nav ul {
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    left: 10%;
+    top: 50%;
+    transform: translate(-5%, -50%);
+  }
+
+  .close img:first-of-type {
+    width: 23px;
+    height: 23px;
+  }
+  .close img:last-of-type {
+    width: 11px;
+    height: 11px;
+  }
+  .navbar nav ul {
+    justify-content: flex-start;
+    align-items: flex-start;
+  }
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,124 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="">
+    <section class="hero">
+      <div>
+        <div>
+          <h5 class="subtext">Young Africans Innovators Award</h5>
+          <h1 class="maintext">
+            the kukah prize for <br />
+            young innovators
+          </h1>
+          <p class="paragraphText">
+            An award of recognition for outstanding young Africans innovators
+            <br />
+            in the field of technology .
+          </p>
+
+          <div class="getstarted">
+            <q-btn flat no-caps class="bg-secondary text-white">
+              Get started
+            </q-btn>
+          </div>
+
+          <div class="smallImgs">
+            <img src="../assets/herosmall1.png" alt="" />
+            <img src="../assets/herosmall2.png" alt="" />
+            <img src="../assets/herosmall3.png" alt="" />
+            <img src="../assets/herosmall4.png" alt="" />
+          </div>
+        </div>
+
+        <div>
+          <img src="../assets/heroframe.png" alt="" />
+        </div>
+      </div>
+
+      <div class="centerimg">
+        <img src="../assets/herocycle.png" alt="" />
+      </div>
+    </section>
+
+    <section class="intro container">
+      <div class="text-center">
+        <h2 class="maintext text-primary">
+          introduction<img src="../assets/introtext.svg" alt="" />
+        </h2>
+        <h5 class="subtext">THE KUKAH PRIZE FOR YOUNG INNOVATORS</h5>
+        <p>
+          An Award of recognition for outstanding innovators in the field of
+          technology presented to young Africans who show exceptional promise as
+          a developing leader in digital transformation. Bright young minds with
+          creative and innovative solutions to developmental challenges both in
+          their communities and the world at large are evaluated by judges in
+          the startup and tech space across the continent for the most
+          innovative digital solutions in the form of startups or initial
+          concepts meeting global developmental challenges.
+        </p>
+      </div>
+
+      <div class="video_area">
+        <iframe
+          width="100%"
+          height="415"
+          src="https://www.youtube.com/embed/l4bDjU-48lE?si=gES7V7GDufU1gjPw"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </section>
+
+    <section class="icon">
+      <div class="container">
+        <div class="text-center">
+          <h2 class="maintext text-white">
+            The<span class="text-secondary"> Icon </span
+            ><img src="../assets/thisicon.svg" alt="" />
+          </h2>
+        </div>
+
+        <div class="grid">
+          <div class="left">
+            <h3>
+              Bishop Matthew <br />
+              Hassan Kukah
+            </h3>
+
+            <p>
+              Bishop Kukah is one of Nigeria’s foremost public intellectuals who
+              has delivered numerous public lectures across Nigeria and
+              worldwide, written several books and hundreds of articles on
+              various aspects of governance, faith, human rights and public
+              policy in Nigeria. His contributions have added moral input,
+              shaped public policy and service. The bishop was responsible for
+              the negotiation and end of the long drawn out conflict between
+              Shell Petroleum Development Corporation and the people of
+              Ogoniland in Rivers State, Nigeria.
+            </p>
+
+            <q-btn flat no-wrap no-caps>
+              Read more
+              <span
+                class="bg-secondary q-ml-md"
+                style="width: 38.596px; height: 38.596px; border-radius: 50%"
+                ><img src="../assets/arrow.svg" alt=""
+              /></span>
+            </q-btn>
+          </div>
+          <div class="right">
+            <img src="../assets/bish.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+});
 </script>
